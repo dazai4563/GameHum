b24d963ecbbb034019e1c137e311ee6caa879d2c
dazai4563 <makselizarov2017@gmail.com>
2026-06-06 01:29:38 +0300

    132

--- config.js
+++ config.js
@@ -3,3 +3,6 @@
 
 const SUPABASE_URL = 'https://ryygwvivzojsnvuzaxpp.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_vJivT1c9MiiFvPZxB7A3sw_XLhKSj8x';
+
+// Создаём клиент и сохраняем в window, чтобы он был доступен всем скриптам
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
